6d696cf9f12ec340cd0089b07511b2594473eda4
NivethaPR <nivethapalanisamy.be@gmail.com>
2017-07-07 12:47:10 +0530

    Add files via upload

---
+++ Unique.java
@@ -0,0 +1,25 @@
+package Guvi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Unique {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the array size : ");
+		int size=s.nextInt();
+		int [] arr=new int[size];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println("enter the index :");
+		int index=s.nextInt();
+		System.out.println("element in the "+index+" position");
+		System.out.println(arr[index]);
+
+	}
+
+}
